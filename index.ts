0427c21e1cc6feaad65a9e3561ebe800d3574d52
xXadri29Xx <adrian29perez@gmail.com>
2025-10-03 14:35:50 +0200

    Rick and Morty getCharacter

--- index.ts
+++ index.ts
@@ -1,13 +1,62 @@
 import axios from "axios";
 
-const laPromesaAxios = axios
-    .get("https://rickandmortyapi.com/api")
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log("Error en la petición: ",err);
-    })
-    .finally(() => {
-        console.log("La promesa ha finalizado su ejecución")
-    })
+const urlbase= 'https://rickandmortyapi.com/api/character/'
+
+const getCharacter = (name?: string, status?: string, gender?: string) => {
+    const variables = [
+        { key: "name", value: name },
+        { key: "status", value: status },
+        { key: "gender", value: gender }
+     ];
+    const urlfinal: (string | undefined) = variables.reduce((acc,i) => {
+    if (i.value) {
+      if (!acc) {
+        acc = `?${i.key}=${i.value}`;
+      } else {
+        acc += `&${i.key}=${i.value}`;
+      }
+    }
+    return acc;
+    }, "")
+
+
+    axios.get(urlbase+urlfinal).then(res=>console.log(res.data.results))
+}
+
+
+getCharacter('Rick','alive','female')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    const finalurl='?name='+name+'&status='+status+'&gender='+gender;
+    axios.get(urlbase+finalurl).then(res=>console.log(res.data.results))
+*/
+
+/*
+getEpisodeofCharacter(idPersonaje)
+
+*/
